f83661006c77d49de153374ef24a455fdea8f48e
rrrkmaurya <rrrkmaurya563@gmail.com>
2020-04-12 01:52:45 +0530

    comment updated

--- londonweather.py
+++ londonweather.py
@@ -3,9 +3,13 @@ import pandas as pd
 import requests
 from pandas import json_normalize
 
-
+'''
+A Class which perform some verification on a API which return the details about
+the London weather
+'''
 class LondonWeather:
 
+    # Method to get the response code of the called API
     def get_API_response(self, URL):
         #     URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
         df = requests.get(url=URL).json()
@@ -14,15 +18,18 @@ class LondonWeather:
     def check_status_code(self, data):
         return int(data['cod'])
 
+    # method to get No of days for which data is available
     def contains_4days_records(self, data):
         return int(data['cnt'])
 
+    # method to find the diff b/w set of times
     def find_time_diff(self, data):
         tempdata = pd.DataFrame.from_dict(data['list'])
         timedata = list(tempdata['dt'])
         res = [timedata[i + 1] - timedata[i] for i in range(len(timedata) - 1)]
         return len(res), len(set(res)), set(res).pop()
 
+    # method to verify value of temp is in the range of temp_min and temp_max
     def verify_temp_with_ref(self, data):
         tempdata = list(pd.DataFrame.from_dict(data['list'])['main'])
         count = 0
@@ -32,6 +39,7 @@ class LondonWeather:
                 count += 1
         return count
 
+    # method to verify the destination based on the ID .
     def verify_description(self, data, bdict):
         #     bdict={500:'light rain', 800:'clear sky'}
         tempdata = pd.DataFrame.from_dict(data['list'])['weather']
--- test_londonweather.py
+++ test_londonweather.py
@@ -5,23 +5,29 @@ lw = LondonWeather()
 API_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
 wData = lw.get_API_response(API_URL)
 
+# check responce code of API
 def test_check_status_code():
     assert lw.check_status_code(wData) == 200
 
+# Is the response contains 4 days of data
 def test_contains_4Days_Records():
     assert lw.contains_4days_records(wData) == 24*4
 
+# 2. Is all the forecast in the hourly interval ( no hour should be missed )
 def test_find_time_diff():
     total_res, unique_val, time_diff = lw.find_time_diff(wData)
     assert total_res == 24*4-1 and unique_val == 1 and time_diff == 3600
 
+# 3. For all 4 days, the temp should not be less than temp_min and not more than temp_max
 def test_verify_temp_with_ref():
     assert lw.verify_temp_with_ref(wData) == 24*4
 
+# 4. If the weather id is 500, the description should be light rain
 def test_verify_description_id500():
     bdict={500:'light rain'}
     assert lw.verify_description(wData, bdict) == True
 
+# 5. If the weather id is 800, the description should be a clear sky
 def test_verify_description_id800():
     bdict={800:'clear sky'}
     assert lw.verify_description(wData, bdict) == True
