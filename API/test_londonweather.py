22de4c7a45d112b42faa1f0e1817d27c049dba39
rrrkmaurya <rrrkmaurya563@gmail.com>
2020-05-06 23:06:05 +0530

    Refactered

--- API/test_londonweather.py
+++ API/test_londonweather.py
@@ -1,5 +1,4 @@
-from londonweather import LondonWeather
-import pytest
+from API.londonweather import LondonWeather
 
 lw = LondonWeather()
 API_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
